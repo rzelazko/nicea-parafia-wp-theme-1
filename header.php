f777762869c33ad90562615563cb92302678a217
Rafal Zelazko <rzelazko@webperfekt.pl>
2011-09-29 15:21:04 +0200

    Mobile profile support

--- header.php
+++ header.php
@@ -51,6 +51,8 @@
 
 	<link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/favicon.ico" type="image/x-icon" />
 	<link rel="bookmark icon" href="<?php bloginfo( 'template_directory' ); ?>/favicon.ico" type="image/x-icon" />
+	
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 		
 	<?php wp_enqueue_script("jquery"); ?>
 	<?php wp_head(); ?>
--- js/homepage.js
+++ js/homepage.js
@@ -2,16 +2,31 @@
 /*jslint white: true, onevar: true, undef: true, nomen: true, regexp: true, plusplus: true, bitwise: true, newcap: true, maxerr: 50, indent: 4 */
 
 jQuery(document).ready(function () {
-	var npCarousel = new NPCarousel(jQuery);
-	npCarousel.init(carouselSettings);
+	var isMobile = jQuery(document).width() < 650,
+		headSearchForm = jQuery('#headSearch');
+		npCarousel = null;
+		
+	if (!isMobile) {
+		npCarousel = new NPCarousel(jQuery);
+		npCarousel.init(carouselSettings);
+	}
 
-    jQuery('#searchIco').click(function () {
-		jQuery('#headSearch').css('right', 'auto');
-		jQuery('#headSearch').css('left',
-			jQuery('#searchIco').position().left
-			- jQuery('#headImage').position().left
-			- parseInt(jQuery('#searchIco').parents('li:first').css('padding-left'), 10));
-
-        jQuery('#headSearch').animate({opacity: 'toggle'});
-    });
+	jQuery('#searchIco').click(function () {
+		if (!isMobile) {
+			headSearchForm.css('left',
+				jQuery('#searchIco').position().left
+				- jQuery('#headImage').position().left
+				- parseInt(jQuery('#searchIco').parents('li:first').css('padding-left'), 10));
+			
+			headSearchForm.animate({opacity: 'toggle'});
+		} else {
+			headSearchForm = headSearchForm.detach();
+			headSearchForm.appendTo('body');
+			
+			headSearchForm.css('top', 
+				jQuery('#searchIco').position().top + jQuery('#searchIco').height());
+				
+			headSearchForm.toggle();
+		}
+	});
 });
